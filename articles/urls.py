e8a69f9f8701368570a9b824cc7000c35ad2fb91
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-06 20:42:37 +0900

    Feat: manage articles urls

---
+++ articles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views    
+
+app_name = 'articles'
+
+urlpatterns = [
+       path('index/', views.index),
+    path('dinner/<str:name>/', views.dinner),
+    path('review/', views.review, name='review'),
+    path('create_review/', views.create_review, name='create_review'),
+]
+    
+ 
