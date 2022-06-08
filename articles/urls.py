b47ec20486d7d8bc7955dee3e041790d6cdc1472
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-08 22:07:28 +0900

    Feat: make urls for detail, edit, update, delete

--- articles/urls.py
+++ articles/urls.py
@@ -4,10 +4,14 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-       path('index/', views.index),
-    path('dinner/<str:name>/', views.dinner),
+    path('index/', views.index),
+    path('dinner/', views.dinner, name='dinner'),
     path('review/', views.review, name='review'),
     path('create_review/', views.create_review, name='create_review'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/update', views.update, name='update'),
 ]
     
  
